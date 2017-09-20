63c2112bf17c1c9635d432f809786a9b9a6436d1
Ashok <abhishekvaid@gmail.com>
2017-09-20 12:51:25 +0530

    commit

--- app/src/main/java/com/example/tabishhussain/hopeorbits/views/activities/accounts/LoginActivity.java
+++ app/src/main/java/com/example/tabishhussain/hopeorbits/views/activities/accounts/LoginActivity.java
@@ -1,16 +1,12 @@
 package com.example.tabishhussain.hopeorbits.views.activities.accounts;
 
-import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
 import com.crashlytics.android.Crashlytics;
 import com.example.tabishhussain.hopeorbits.R;
-import com.example.tabishhussain.hopeorbits.vendor.BusinessPage;
 import com.example.tabishhussain.hopeorbits.views.activities.BaseActivity;
-import com.example.tabishhussain.hopeorbits.views.fragments.accounts.LoginFragment;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -28,15 +24,15 @@ public class LoginActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_content);
-        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
-        Login = sharedpreferences.getBoolean("login", false);
-        if (Login) {
-            startActivity(new Intent(LoginActivity.this, BusinessPage.class));
-        } else {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.frame, new LoginFragment()).commit();
-            setUpToolbar("Login", false);
-        }
+//        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+//        Login = sharedpreferences.getBoolean("login", false);
+//        if (Login) {
+//            startActivity(new Intent(LoginActivity.this, BusinessPage.class));
+//        } else {
+//            getSupportFragmentManager().beginTransaction()
+//                    .replace(R.id.frame, new LoginFragment()).commit();
+//            setUpToolbar("Login", false);
+//        }
 
 
     }
